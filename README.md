2b12941fab0c780a96d821b3b8efd07e694bc78c
Johan Ronsse <johan.ronsse@gmail.com>
2016-09-19 14:33:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Bedrock Mini
+
+### A mini prototyping setup
+
+One step up from Codepen - work in your local environment using the handiness of Jade and SCSS with autoprefixing.
+
+* Renders Jade templates
+* Has Browsersync starting a local server
+* Compiles SCSS
+* Already has references to jQuery in place, and a script tag to begin your JS
+
+If you need more power, a styleguide, better navigation between templates, consider <a href="http://bedrock.mono.company/">Bedrock</a>.
+
+### Instructions
+
+* Put your Jade files in `src`
+* Put your SCSS files in `scss`
+
+* Run `npm install` then `gulp` to start working
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+
+var jade = require('gulp-jade');
+var autoprefixer = require('gulp-autoprefixer');
+
+var rename = require('gulp-rename');
+var replace = require('gulp-replace-task');
+var sass = require('gulp-sass');
+
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('sass', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('./dist/'))
+});
+
+/**
+ * Important!!
+ * Separate task for the reaction to `.jade` files
+ */
+gulp.task('jade-watch', ['templates'], reload);
+
+gulp.task('templates', function() {
+  
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('./src/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(browserSync.stream())
+    .pipe(gulp.dest('./dist/'))
+
+});
+
+/**
+ * Serve and watch the scss/jade files for changes
+ */
+gulp.task('default', ['sass', 'templates'], function () {
+
+    browserSync({
+      server: './dist',
+      notify: false
+    });
+
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./src/*.jade',  ['jade-watch']);
+});
+
